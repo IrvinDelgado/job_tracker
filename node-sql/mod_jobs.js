fafae7562aaaf7a2ec1f8f47a00706e2b133c41c
IrvinDelgado <Idelgadoramirez@hawk.iit.edu>
2021-01-02 15:33:06 -0600

    added openDB

--- node-sql/mod_jobs.js
+++ node-sql/mod_jobs.js
@@ -1,12 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
 
+const openDB = async() =>{
+    const db = await open({
+        filename: 'C:\\Users\\ID_11\\job_tracker',
+        driver: sqlite3.Database
+    });
+    return db;
+}
+
 const read_Jobs = async()=> {
     // open the database
-    const db = await open({
-      filename: 'C:\\Users\\ID_11\\job_tracker',
-      driver: sqlite3.Database
-    });
+    const db = await openDB();
     const result = await db.all('SELECT * FROM jobs');
     db.close();
     return result
@@ -14,10 +19,7 @@ const read_Jobs = async()=> {
 
 const add_Job = async(job) => {
     //  Connect to DB
-    const db = await open({
-        filename: 'C:\\Users\\ID_11\\job_tracker',
-        driver: sqlite3.Database
-    });
+    const db = await openDB();
     let col = '(jobNumber, company, position, location, website, applied, stage, result, date)'
     let valParm = '(?,?,?,?,?,?,?,?,?)'
     let valArr = []
@@ -34,16 +36,12 @@ const add_Job = async(job) => {
 }
 
 const update_Job = async(job) =>{
-    const db = await open({
-        filename: 'C:\\Users\\ID_11\\job_tracker',
-        driver: sqlite3.Database
-    });
-    console.log(job);
-    let sql_param = [job.jobNumber,job.company,job.position,job.location,job.website,job.applied,job.stage,job.result,job.date]
-    let col_set = 'jobNumber = ?, company = ?, position = ?, location = ?, website = ?, applied = ?, stage = ?, result = ?, date = ?'
+    const db = await openDB();
+    let sql_param = [job.company,job.position,job.location,job.website,job.applied,job.stage,job.result,job.date,job.jobNumber]
+    let col_set = 'company = ?, position = ?, location = ?, website = ?, applied = ?, stage = ?, result = ?, date = ?'
     let sql =   `UPDATE jobs\
                 SET ${col_set}\
-                WHERE jobNumber = ${job.jobNumber}`;
+                WHERE jobNumber = ?`;
     db.run(sql, sql_param, function(err) {
         if (err) {
             return console.log(err.message);
@@ -53,10 +51,7 @@ const update_Job = async(job) =>{
 }
 
 const del_Job = async(job_id) =>{
-    const db = await open({
-        filename: 'C:\\Users\\ID_11\\job_tracker',
-        driver: sqlite3.Database
-    });
+    const db = await openDB();
     let sql = `DELETE FROM jobs WHERE jobNumber = ?`
     db.run(sql, [job_id], function(err) {
         if (err) {
